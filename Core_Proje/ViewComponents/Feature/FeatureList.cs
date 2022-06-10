031f09aa0f571421d33897443d05d86d9209d7f2
Egemen Karataş <segemen.karatas@gmail.com>
2022-06-10 22:01:47 +0300

    Feature List

---
+++ Core_Proje/ViewComponents/Feature/FeatureList.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Core_Proje.ViewComponents.Feature
+{
+    public class FeatureList:ViewComponent
+    {
+
+
+        FeatureManager fm = new FeatureManager(new EfFeatureDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = fm.TGetList();
+            return View(values);
+        }
+    }
+}
