9ef8e326a4fbf1c3f03270c3a232718c639c7aec
MrBlackStone <segemen.karatas@gmail.com>
2022-06-29 14:32:45 +0300

    Skills

--- Core_Proje/Controllers/SkillController.cs
+++ Core_Proje/Controllers/SkillController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
 {
     public class SkillController : Controller
     {
+        SkillManager sm = new SkillManager(new EfSkillDal());
         public IActionResult Index()
         {
+            var values = sm.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddSkill(Skill skill)
+        {
+
+            sm.TAdd(skill);
+            return RedirectToAction("Index");
         }
     }
 }
