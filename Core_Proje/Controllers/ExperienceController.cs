9c5015268db4ebd72301698236094c717af24a10
MrBlackStone <segemen.karatas@gmail.com>
2022-07-08 13:28:23 +0300

    test

---
+++ Core_Proje/Controllers/ExperienceController.cs
@@ -0,0 +1,57 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        ExperienceManager em = new ExperienceManager(new EfExperienceDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Deneyim Listesi";
+            ViewBag.v2 = "Deneyimler";
+            ViewBag.v3 = "Deneyim Listesi";
+
+            var values = em.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddExperience()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddExperience(Experience p)
+        {
+            em.TAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteExperience(int id)
+        {
+            var values = em.TGetByID(id);
+            em.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditExperience(int id)
+        {
+            ViewBag.v1 = "Deneyim DÜzenleme";
+            ViewBag.v2 = "Deneyimler";
+            ViewBag.v3 = "Deneyim DÜzenleme";
+            var values = em.TGetByID(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult EditExperience(Experience p)
+        {
+
+            em.TUpdate(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
